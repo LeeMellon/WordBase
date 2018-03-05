55c1ce3a85d533a4f5ad9b9b92c34063a01ba06f
Frank Ngo and Sara Hamilton <sara@>
2018-03-05 15:37:19 -0800

    add ability to switch between player turns

--- Wordbase/wwwroot/js/scripts.js
+++ Wordbase/wwwroot/js/scripts.js
@@ -1,5 +1,10 @@
 var word = "";
 var cells = [];
+var playerTurn = new PlayerTurn;
+
+function PlayerTurn(){
+  this.player = 0;
+}
 
 function randomChar(){
   var text = "";
@@ -11,13 +16,24 @@ function randomChar(){
 
 function myFunction(square)
 {
-  var playerTurn = 1;
-  if(playerTurn == 1)
-  {
-    var x = document.getElementById(square).innerHTML;
-    var y = document.getElementById(square).id;
-    document.getElementById(square).style.backgroundColor = "#BB8FCE"
-    playerTurn == 2;
+  // var playerTurn = 1;
+  var player = playerTurn.getPlayer();
+  var x = document.getElementById(square).innerHTML;
+  var y = document.getElementById(square).id
+  if(!cells.includes(y)) {
+      if(player == 1)
+      {
+        document.getElementById(square).style.backgroundColor = "#BB8FCE";
+      }
+      else if (player == 2)
+      {
+        document.getElementById(square).style.backgroundColor = "#F1948A";
+      }
+      else
+      {
+        console.log("Not a turn");
+      }
+    this.onclick=null;
     word += x;
     newword.append(x);
     cells.push(y);
@@ -26,8 +42,17 @@ function myFunction(square)
   }
 }
 
+PlayerTurn.prototype.getPlayer = function(player) {
+  return playerTurn.player;
+}
 
-$(function() {
+PlayerTurn.prototype.setPlayer = function(player) {
+  playerTurn.player = player;
+}
+
+
+$(document).ready(function() {
+  playerTurn.setPlayer(1);
   var letters = ["A","B","C","D","E","F","G","H","I","J"];
   for(var i  = 1; i <= 13; i++)
   {
@@ -38,8 +63,25 @@ $(function() {
       element.innerHTML = newChar;
     }
   }
-
-
-  // $("#turnEnd").click()
-
+  $("#turnEnd").click(function(event){
+    event.preventDefault();
+    // run a bunch of checks here for valid play
+    // store play data in database
+    if (playerTurn.getPlayer() == 1)
+    {
+      console.log("Player 1 Turn End");
+      playerTurn.setPlayer(2);
+      $(".turn1").show();
+      $(".turn2").hide();
+    }
+    else if (playerTurn.getPlayer() == 2)
+    {
+      playerTurn.setPlayer(1);
+      console.log("Player 2 Turn End");
+      $(".turn2").show();
+      $(".turn1").hide();
+    }
+    word = "";
+    cells = [];
+  });
 });
