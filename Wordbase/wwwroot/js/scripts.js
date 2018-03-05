8b726bcd267751726d161f344d7728ba395638b5
Frank Ngo and Sara Hamilton <sara@>
2018-03-05 11:13:58 -0800

    add random word gen and board

--- Wordbase/wwwroot/js/scripts.js
+++ Wordbase/wwwroot/js/scripts.js
@@ -1,4 +1,24 @@
+function randomChar(){
+  var text = "";
+  var possible = "AAAAAAAABBCCDDDDEEEEEEEEFFGGGHHIIIIIIIIIJKLLLLMMNNNNOOOOOOPPQRRRRRRSSSSTTTTTUUUUVVWWXYYZ";
+
+  text = possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
+
+
+
+
 $(function() {
 
-
+  var letters = ["A","B","C","D","E","F","G","H","I","J"];
+  for(var i  = 1; i <= 13; i++)
+  {
+    for( var j = 0;j<letters.length; j++)
+    {
+      var newChar = randomChar()
+      var element = document.getElementById(letters[j]+','+i);
+      element.innerHTML = newChar;
+    }
+  }
 });
